ba053a73228ba385d98ac49d3105ff0b939e51cb
Hung Dang <hungdt3@gmail.com>
2025-03-28 17:48:22 +0700

    addd

--- src/main/java/com/example/send_email_service/service/impl/SendEmailServiceImpl.java
+++ src/main/java/com/example/send_email_service/service/impl/SendEmailServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.send_email_service.service.impl;
 import com.example.send_email_service.service.SendEmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -17,8 +19,12 @@ public class SendEmailServiceImpl implements SendEmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    private static final Logger logger = LoggerFactory.getLogger(SendEmailServiceImpl.class);
+
     @Override
     public void sendEmail(String to, String subject, String text) throws UnsupportedEncodingException, MessagingException {
+        logger.info("[START] Send to: {}", to);
+
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
@@ -26,7 +32,9 @@ public class SendEmailServiceImpl implements SendEmailService {
         helper.setSubject(subject);
         helper.setText(text, true);
         helper.setFrom("hungdt3@gmail.com", "Đặng Thế Hưng"); // Đặt tên người gửi
-        helper.setReplyTo("dangthehung1999@gmail.com"); // Đặt email phản hồi
+        helper.setReplyTo("test.hungdt379@gmail.com"); // Đặt email phản hồi
         mailSender.send(message);
+
+        logger.info("[END  ] Send to: {}", to);
     }
 }
